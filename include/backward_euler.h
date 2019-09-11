c98a10c5324e3581d9d7fd2fcae73613074e4cfb
David Levin <dilevin@csail.mit.edu>
2019-09-11 11:39:13 -0400

    more comments

--- include/backward_euler.h
+++ include/backward_euler.h
@@ -1,7 +1,16 @@
 #include <Eigen/Dense>
 
-//force - function which computes the gradient of the potential energy function as a function of the state (q, qdot).
-//stiffness - function that computes the stiffness (- hessian ) of the potential energy
+//Input:
+//  q - generalized coordiantes for the mass-spring system
+//  qdot - generalized velocity for the mass spring system
+//  dt - the time step in seconds
+//  mass - the mass
+//  force(q, qdot) - a function that computes the force acting on the mass as a function. This takes q and qdot as parameters.
+//  stiffness(q, qdot) - a function that computes the stiffness (negative second derivative of the potential energy). This takes q and qdot as parameters.  
+//Output:
+//  q - set q to the updated generalized coordinate using Backward Euler time integration
+//  qdot - set qdot to the updated generalized velocity using Backward Euler time integration
+
 template<typename FORCE, typename STIFFNESS> 
 inline void backward_euler(Eigen::VectorXd &q, Eigen::VectorXd &qdot, double dt, double mass,  FORCE &force, STIFFNESS &stiffness) {
 
